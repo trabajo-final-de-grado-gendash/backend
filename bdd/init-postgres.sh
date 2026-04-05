1fd3a618b4f4663c41ebf6bdd8ecbcfa0554678f
NahuelCarro <nahuelagustincarro@gmail.com>
2026-04-04 23:18:49 -0300

    feat: subo la bdd y un inicializador de la bdd

---
+++ bdd/init-postgres.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+echo "Ensuring the chinook database exists..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    SELECT 'CREATE DATABASE chinook'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'chinook')\gexec
+EOSQL
+
+echo "Loading chinook schema into the chinook database..."
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "chinook" -f /tmp/chinook.sql
+echo "Chinook schema loaded successfully!"
